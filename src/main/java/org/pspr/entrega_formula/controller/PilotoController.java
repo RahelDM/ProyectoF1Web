3f6fe3b225a7bf0cc51f986ba7475e74f9a4c798
Raquel Suárez <raquelsa34@educastur.es>
2024-02-18 16:18:16 +0100

    Creaciónd de pilotos completada.

--- src/main/java/org/pspr/entrega_formula/controller/PilotoController.java
+++ src/main/java/org/pspr/entrega_formula/controller/PilotoController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Random;
 
 @Controller
 @RequestMapping("/pilotos")
@@ -20,47 +19,56 @@ public class PilotoController {
     private PilotoService pilotoService;
 
     @GetMapping("")
-    public String getPilotos(Model model){
+    public String getPilotos(Model model) {
         List<Piloto> pilotoList = pilotoService.findAllPilotos();
-        model.addAttribute("pilotos",pilotoList);
+        model.addAttribute("pilotos", pilotoList);
         return "piloto/pilotos";
     }
 
     @GetMapping("/{id}")
-    public String getPilotoById(Model model, @PathVariable Long id){
+    public String getPilotoById(Model model, @PathVariable Long id) {
         Optional<Piloto> pilotoOptional = pilotoService.findPilotoById(id);
-        if(pilotoOptional.isPresent()){
-            model.addAttribute("piloto",pilotoOptional.get());
-        }else {
-            model.addAttribute("msg","Sin pilotos con id: " + id);
+        if (pilotoOptional.isPresent()) {
+            model.addAttribute("piloto", pilotoOptional.get());
+        } else {
+            model.addAttribute("msg", "Sin pilotos con id: " + id);
         }
         return "piloto/piloto";
     }
 
 
     @GetMapping("/pilotoForm/{id}")
-    public String updatePiloto(Model model, @PathVariable Long id){
+    public String updatePiloto(Model model, @PathVariable Long id) {
         Optional<Piloto> pilotoOptional = pilotoService.findPilotoById(id);
-        if(pilotoOptional.isPresent()){ //el piloto a actualizar existe
-            model.addAttribute("pilotoParaActualizar",pilotoOptional.get()); //mandamos el piloto para actualizar
+        if (pilotoOptional.isPresent()) { //el piloto a actualizar existe
+            model.addAttribute("piloto", pilotoOptional.get()); //mandamos el piloto para actualizar
             model.addAttribute("nuevo", false);
             return "piloto/pilotoForm";
-        }else {
-            model.addAttribute("msg","No hay un piloto con id: " + id);
+        } else {
+            model.addAttribute("msg", "No hay un piloto con id: " + id);
         }
         return "redirect:/pilotos"; //si se produce algún error y no se accede al formulario...
     }
 
+    @GetMapping("/piloto/add")
+    public String savePiloto(Model model) {
+        Piloto piloto = new Piloto();
+        model.addAttribute("piloto", piloto);
+        model.addAttribute("nuevo", true);
+        return "piloto/pilotoForm";
+    }
+
+
     @PostMapping("/piloto/save")
-    public String savePiloto(@ModelAttribute(value="pilotoFormulario") Piloto pilotoFormulario){
+    public String savePiloto(@ModelAttribute(value = "pilotoFormulario") Piloto pilotoFormulario) {
         pilotoService.save(pilotoFormulario);
-        return "redirect:/pilotos/"+pilotoFormulario.getPilotoId(); //si se produce algún error y no se accede al formulario...
+        return "redirect:/pilotos/" + pilotoFormulario.getPilotoId(); //si se produce algún error y no se accede al formulario...
     }
 
     @PutMapping("/piloto/save")
-    public String updatePiloto(@ModelAttribute(value="pilotoFormulario") Piloto pilotoFormulario){
+    public String updatePiloto(@ModelAttribute(value = "pilotoFormulario") Piloto pilotoFormulario) {
         pilotoService.save(pilotoFormulario);
-        return "redirect:/pilotos/"+pilotoFormulario.getPilotoId(); //si se produce algún error y no se accede al formulario...
+        return "redirect:/pilotos/" + pilotoFormulario.getPilotoId(); //si se produce algún error y no se accede al formulario...
     }
 
 
